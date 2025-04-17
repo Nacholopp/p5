2547ecc66809229be7f58508c6cc38c9e98caff5
nacho <nacholopez082@gmail.com>
2025-04-17 21:52:49 +0200

    TODO#3 HECHO

--- src/main/java/edu/comillas/icai/gitt/pat/spring/p5/entity/Token.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/p5/entity/Token.java
@@ -1,12 +1,15 @@
 package edu.comillas.icai.gitt.pat.spring.p5.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import jakarta.persistence.OneToOne;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+
+
 
 /**
  * TODO#3
@@ -17,7 +20,18 @@ import org.hibernate.annotations.OnDeleteAction;
  */
 
 @Entity
+@Builder //Genera los constructores
+@Data //Genera los getters y setters
+@NoArgsConstructor  // constructor vacío requerido por JPA
+@AllArgsConstructor // necesario para que @Builder funcione correctamente si no dan fallos
+@Table(name = "TOKEN") //para decir a que tabla pertenece
 public class Token {
+    @Column(name = "ID")
     @Id @GeneratedValue(strategy = GenerationType.UUID) public String id;
 
+    @JoinColumn(name = "appUser", referencedColumnName = "ID", nullable = false) //Crea la columna de clave foránea de appUser, es decir coge la clave principal y la asocia a esta columna
+    @OneToOne //sin especificar el tipo de fetch
+    @OnDelete(action = OnDeleteAction.CASCADE) //Elimina el Token si el AppUser es eliminado
+    private AppUser appUser; //es de tipo AppUser porque representa la relación entre las 2 entidades con un string no se estaría relacionando
+    //Esto me permite acceder a todos los datos del usuario relacionado más tarde
 }
